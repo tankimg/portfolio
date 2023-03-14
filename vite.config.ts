168a7adfd956f9b59d8611de6348a8e2d0e24530
tankimg <tangui.marchandguerniou@gmail.com>
2023-03-14 23:53:08 +0100

    update

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     build:{
-        chunkSizeWarningLimit: 800,
-    }
+        chunkSizeWarningLimit: 800
+    },
+    base: '/portfolio/'
 });
